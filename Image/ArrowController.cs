a24ea700e0c112e9117ab2f7010797879a55a1ac
Cody <musoudeus@gmail.com>
2023-08-19 16:42:06 +0800

    manipulate the ArrowController to let it have a function to deal with collision

--- Image/ArrowController.cs
+++ Image/ArrowController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ArrowController : MonoBehaviour{
+
+    GameObject player;
     void Start(){
+        this.player = GameObject.Find("player");
     }
     void Update(){
         // 每個影格等速往下移動
@@ -13,5 +16,21 @@ public class ArrowController : MonoBehaviour{
         if(transform.position.y < -5.0f){
             Destroy(gameObject);
         }
+
+        Vector2 p1 = transform.position;
+        Vector2 p2 = this.player.transform.position;
+        Vector2 dir =  p1 - p2;
+
+        float d = dir.magnitude;
+        float r1 = 0.5f; // 箭頭的圓半徑
+        float r2 = 1.0f; // 遊戲角色的圓半徑
+
+        if(d < r1 + r2){
+            // 發生衝突時就捨棄箭頭
+            Destroy(gameObject);
+        }
+
+
+
     }
 }
